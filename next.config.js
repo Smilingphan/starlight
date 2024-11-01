e2828182cc8c8738573c0f2af4bfb3d3906f4a77
Smilingphan <phanthibichtran05@gmail.com>
2024-11-01 10:51:35 +0700

    Make Profile Image Large

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true, // Bật chế độ strict của React
+    images: {
+        domains: ['your-image-domain.com'], // Thêm các domain của hình ảnh nếu bạn sử dụng hình ảnh từ bên ngoài
+    },
+    env: {
+        // Nếu bạn có các biến môi trường, có thể định nghĩa ở đây
+        CUSTOM_ENV_VARIABLE: process.env.CUSTOM_ENV_VARIABLE,
+    },
+};
 
 module.exports = nextConfig;
